caaed886b53a688f036792f3010c8c96d978d717
Rafaela Coelho Bastos <rafaelacoelhobastos@gmail.com>
2025-11-19 20:51:50 -0300

    ajustes

--- src/Projeto finalizado/entregas-tabalho-main/entregas-tabalho-main/frontend/dashboard/src/components/Sidebar.jsx
+++ src/Projeto finalizado/entregas-tabalho-main/entregas-tabalho-main/frontend/dashboard/src/components/Sidebar.jsx
@@ -1,19 +1,19 @@
 import { Link } from "react-router-dom";
+
 export default function Sidebar() {
   return (
-    
-    <aside className="w-64 h-full bg-white shadow-lg flex flex-col p-6 text-gray-800">
+    <aside className="w-64 min-h-screen bg-white shadow-lg flex flex-col p-6 text-gray-800">
       <h2 className="text-xl font-semibold mb-6">Menu</h2>
       <nav className="flex flex-col gap-3">
-      <Link to="/overview" className="hover:text-pink-500 transition">
-  Visão Geral
-</Link>
-<Link to="/dashboard" className="hover:text-pink-500 transition">
-  Relatórios
-</Link>
-<Link to="/settings" className="hover:text-pink-500 transition">
-  Configurações
-</Link>
+        <Link to="/overview" className="hover:text-pink-500 transition">
+          Visão Geral
+        </Link>
+        <Link to="/dashboard" className="hover:text-pink-500 transition">
+          Relatórios
+        </Link>
+        <Link to="/settings" className="hover:text-pink-500 transition">
+          Configurações
+        </Link>
       </nav>
     </aside>
   );
